8984f59fa241cb3c1379c73cf9993fdeb8b1f480
Elena Ackovska <elena.ackovska24@gmail.com>
2021-02-01 16:40:52 +0100

    Added week-3 folder

---
+++ JavaScript-Module-1/week-3/js/exercise-1.js
@@ -0,0 +1,1 @@
+// 1. Write a console.log statement saying "Hello World!" for each language that you know.
---
+++ JavaScript-Module-1/week-3/js/exercise-9.js
@@ -0,0 +1,13 @@
+// 9.1 9.2 9.3 9.4 9.5 9.6 9.7
+let remainder = 7;
+remainder = remainder % 3;
+console.log(' The value of remainder will be 1');
+console.log(remainder);
+
+console.log(
+  'The remainder operator (%) returns the remainder left over when one operand is divided by a second operand'
+);
+console.log(
+  'The reminder of 11 % 3 => 11 / 3 (returns the integer part of the division(in this case will be 3) and the reminder will be 2)'
+);
+console.log('The reminder of 15 / 4 will be 3');
