afe1688ae879dea5951e0a458dde3c828b20a700
Elena Ackovska <elena.ackovska24@gmail.com>
2021-03-04 18:09:32 +0100

    Added files for week-2 homework

---
+++ JavaScript-Module-2/Week-2/maartjes-work.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180,
+  },
+  {
+    name: 'Some web development',
+    duration: 120,
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20,
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200,
+  },
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 240,
+  },
+  {
+    name: 'Some more web development',
+    duration: 180,
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10,
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200,
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40,
+  },
+];
+
+const maartjesTasks = monday.concat(tuesday);
+const maartjesHourlyRate = 20;
+
+function computeEarnings(tasks, hourlyRate) {
+  // console.log(tasks, hourlyRate);
+  const earnings = tasks
+    .map(day => day.duration / 60)
+    .filter(duration => duration >= 2)
+    .map(duration => duration * hourlyRate)
+    .reduce((sum, duration) => {
+      sum += duration;
+      return sum;
+    }, 0);
+  return earnings;
+}
+
+// eslint-disable-next-line no-unused-vars
+const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+
+// add code to convert `earnings` to a string rounded to two decimals (euro cents)
+
+console.log(`Maartje has earned €${earnings.toFixed(2)}`);
+
+// Do not change or remove anything below this line
+module.exports = {
+  maartjesTasks,
+  maartjesHourlyRate,
+  computeEarnings,
+};
