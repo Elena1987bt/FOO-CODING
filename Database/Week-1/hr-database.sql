4192e2af2da3e71ee610075e719a787545791139
Dejene Daba <dejenedaba269@gmail.com>
2021-05-27 09:57:39 +0100

    removed unique key and add foreign key on employee table

--- Database/Week-1/hr-database.sql
+++ Database/Week-1/hr-database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `hr` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `hr`;
 -- MySQL dump 10.13  Distrib 8.0.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hr
@@ -31,7 +29,8 @@ CREATE TABLE `employee` (
   `age` int(11) NOT NULL,
   `city_id` int(11) NOT NULL,
   PRIMARY KEY (`employee_id`),
-  UNIQUE KEY `employee_id_UNIQUE` (`employee_id`)
+  KEY `FOREIGN KEY_idx` (`city_id`),
+  CONSTRAINT `FOREIGN KEY` FOREIGN KEY (`city_id`) REFERENCES `locations` (`city_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -46,27 +45,29 @@ INSERT INTO `employee` VALUES (1,'John','Smith',30,5),(2,'Elena','Ackovska',33,7
 UNLOCK TABLES;
 
 --
--- Table structure for table `location`
+-- Table structure for table `locations`
 --
 
-DROP TABLE IF EXISTS `location`;
+DROP TABLE IF EXISTS `locations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `location` (
-  `city_id` int(11) NOT NULL,
+CREATE TABLE `locations` (
+  `city_id` int(11) NOT NULL AUTO_INCREMENT,
   `city_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`city_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `country` varchar(45) NOT NULL,
+  PRIMARY KEY (`city_id`),
+  UNIQUE KEY `city_id_UNIQUE` (`city_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `location`
+-- Dumping data for table `locations`
 --
 
-LOCK TABLES `location` WRITE;
-/*!40000 ALTER TABLE `location` DISABLE KEYS */;
-INSERT INTO `location` VALUES (1,'London'),(2,'Malmo'),(3,'Berlin'),(4,'Skopje'),(5,'Paris'),(6,'New York'),(7,'Bitola'),(8,'Atina'),(9,'Belgrad'),(10,'Toronto');
-/*!40000 ALTER TABLE `location` ENABLE KEYS */;
+LOCK TABLES `locations` WRITE;
+/*!40000 ALTER TABLE `locations` DISABLE KEYS */;
+INSERT INTO `locations` VALUES (1,'London','UK'),(2,'Malmo','Sweden'),(3,'Berlin','Germany'),(4,'Skopje','Macedonia'),(5,'Paris','France'),(6,'New York','USA'),(7,'Bitola','Macedonia'),(8,'Atina','Greece'),(9,'Belgrad','Srbia'),(10,'Toronto','Canada'),(11,'Barcelona','Spain'),(12,'Oslo','Norway');
+/*!40000 ALTER TABLE `locations` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -78,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-26 11:27:09
+-- Dump completed on 2021-05-26 15:40:58
