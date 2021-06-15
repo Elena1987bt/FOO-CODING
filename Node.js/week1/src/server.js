fa55a465ccc37c7d30c00895d2e11d3c17b8b21c
Dejene Daba <dejenedaba269@gmail.com>
2021-06-15 10:54:12 +0100

    Added homework for node.js week-1

---
+++ Node.js/week1/src/server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const http = require('http');
+
+/* `createServer` MUST return an instance of `http.Server` otherwise the tests
+ * will fail.
+ */
+function createServer(port) {
+  let state = 10;
+
+  const server = http.createServer((request, response) => {
+    // TODO: Write your homework code here
+    switch (request.url) {
+      case '/state':
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({
+          state: state
+        }));
+        break;
+      case '/add':
+        state = state + 1;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state: state}));
+        break;
+      case '/subtract':
+        state = state - 1;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state: state}));
+        break;
+      case '/reset':
+        state = 10;
+        response.setHeader('Content-Type', 'application/json');
+        response.write(JSON.stringify({ state: state }));
+        break;
+      default:
+        if (request.url === '/') {
+          response.statusCode = 302;
+          response.setHeader('Location', '/');
+        }
+        else {
+          response.setHeader('Content-Type', 'application/json');
+          response.statusCode = 404;
+          response.write(JSON.stringify({'error': 'Not found' }));
+        }
+    }
+    response.end();
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer
+};
